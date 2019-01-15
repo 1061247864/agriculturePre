573cb206948eece93b4b59733689bfa7b2f8cae6
luzhilong <loveluzhilong@qq.com>
2019-01-15 08:19:46 +0800

    add login method

--- res/static/js/publicValue.js
+++ res/static/js/publicValue.js
@@ -10,6 +10,7 @@ var titleObj ="<div class=\"site-nav-bg\"  id=\"apps\">\n" +
     "        </p>\n" +
     "        <div class=\"sn-quick-menu\">\n" +
     "            <div class=\"login\"  v-if='!userobj'><a href=\"login.html\">登录</a></div>\n" +
+    "<div class=\"login\"  v-if='!userobj'><a href=\"registry.html\">注册</a></div>\n" +
     "             <div class=\"login\" v-show=\"true\" v-if='userobj' ><a href='javascript:void(0)'>{{userobj.userName}}</a></div>\n       " +
     "            <div class=\"sp-cart\" v-if='userobj' >  <a href=\"shopcart.html\">购物车</a><span>2</span></div>\n" +
     "            <div style='padding-left: 20px' class=\"logout\" v-show=\"true\" v-if='userobj' ><a href='javascript:void(0)'  @click='logout'>退出登录</a></div>\n             " +
